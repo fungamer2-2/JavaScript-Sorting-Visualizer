37b1399dda11d77a75aa82c5250554d275612694
fungamer2-2 <56766520+fungamer2-2@users.noreply.github.com>
2020-09-12 19:02:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 alert("Welcome to fungamer2's Sorting Visualizer!\nNote: This visualizer is in pre-alpha and is not fully developed");
+
+function render(array) {
+    
+}
+
+function generateArray(length) {
+    var array = new Array(length);
+    for (var i = 0; i < length; i++) {
+        array[i] = i + 1;
+    }
+    return array;
+}
+
+function shuffleArray(array) {
+    for (var i = 0; i < length - 1; i++) {
+        var j = i + Math.floor(Math.random() * (length - i));
+        var tmp = array[i]; array[i] = array[j]; array[j] = tmp;
+    }
+}
