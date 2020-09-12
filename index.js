e685ec7d99d163444f5af6c27806b557edb76e91
fungamer2-2 <56766520+fungamer2-2@users.noreply.github.com>
2020-09-12 18:48:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+var canvas = document.getElementById("renderer");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 alert("Welcome to fungamer2's Sorting Visualizer!\nNote: This visualizer is in pre-alpha and is not fully developed");
