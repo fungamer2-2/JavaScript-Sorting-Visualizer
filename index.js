faeac374df4c338f987d10ef8cd8abae3f2ba829
fungamer2-2 <56766520+fungamer2-2@users.noreply.github.com>
2020-09-12 18:20:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-alert("Welcome to fungamer2's Sorting Visualizer!\nNote: This visualizer is in pre-alpha stage");
+alert("Welcome to fungamer2's Sorting Visualizer!\nNote: This visualizer is in pre-alpha and is not fully developed");
