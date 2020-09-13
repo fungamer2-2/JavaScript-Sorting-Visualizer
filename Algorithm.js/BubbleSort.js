e4710214608246f83486dd56fa49c57d2d0a9b7c
fungamer2-2 <56766520+fungamer2-2@users.noreply.github.com>
2020-09-13 17:21:29 -0400

    Update BubbleSort.js

--- Algorithm.js/BubbleSort.js
+++ Algorithm.js/BubbleSort.js
@@ -2,6 +2,12 @@ class BubbleSort {
     static name = "Bubble Sort";
 
     static run(array) {
-        
+        var sorted = false;
+        for (var i = array.length - 1; i > 0; i--) {
+            for (var j = 0; j < i; j++) {
+                mark(j, j + 1)
+                render(array);
+            }
+        }
     }
 }
