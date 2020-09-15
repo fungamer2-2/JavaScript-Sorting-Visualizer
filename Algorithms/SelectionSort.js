34b1fa8a33e5af542a15f0f255d7ab3be0bc4652
fungamer2-2 <56766520+fungamer2-2@users.noreply.github.com>
2020-09-15 13:33:49 -0400

    Update SelectionSort.js

--- Algorithms/SelectionSort.js
+++ Algorithms/SelectionSort.js
@@ -7,11 +7,13 @@ SelectionSort.run = async function(array) {
     for (var i = 0; i < array.length - 1; i++) {
         var min = i;
         for (var j = i; j < array.length; j++) {
-            mark(1, i);
-            mark(1, min);
+            mark(1, j);
+            mark(2, min);
             if (array[i] < array[min]) min = i;
             sleep();
         }
+        mark(1, i);
+        mark(2, min);
         swap(array, i, min);
         sleep();
     }
