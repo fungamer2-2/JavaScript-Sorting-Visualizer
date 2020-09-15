7bdfaf24433ede3c38af96e0438bc47c86458ed2
fungamer2-2 <56766520+fungamer2-2@users.noreply.github.com>
2020-09-15 12:17:43 -0400

    Create SelectionSort.js

---
+++ Algorithms/SelectionSort.js
@@ -0,0 +1,18 @@
+class SelectionSort {
+    static name = "Bubble Sort";
+}
+
+SelectionSort.run = async function(array) {
+    var sorted = false;
+    for (var i = 0; i < array.length - 1; i++) {
+        var min = i;
+        for (var j = i; j < array.length; j++) {
+            mark(1, i);
+            mark(1, min);
+            if (array[i] < array[min]) min = i;
+            sleep();
+        }
+        swap(array, i, min);
+        sleep();
+    }
+}
